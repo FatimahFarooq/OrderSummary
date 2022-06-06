44cc41cdb5ec969c9dab782c93961987467972bb
FatimahFarooq <47738677+FatimahFarooq@users.noreply.github.com>
2022-06-06 17:08:16 +0300

    pushing OrderSummary version 1

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          10: "#1f2f56",
+          paleBlue: "#e0e8ff",
+          brightBlue: "#3829e0",
+          veryPaleBlue: "#f5f7ff",
+          desaturatedBlue: "#7280a7",
+          lightPurple: "#d6e1ff",
+          shadowColor: "#3829e047",
+        },
+      },
+      fontFamily: {
+        RedHatDisplay: "Red Hat Display, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
